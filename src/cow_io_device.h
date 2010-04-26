9eeed3d224d4ac3f18f39784b83e2a94cbb8b2b6
tengil <devnull@localhost>
2010-04-26 21:06:27 +0200

    msvc build fixes

--- src/cow_io_device.h
+++ src/cow_io_device.h
@@ -5,9 +5,9 @@
 // of io_device
 
 #ifdef _WIN32
-    #include <win32/win32_io_device.h>
+    #include "win32/win32_io_device.h"
 #else
-    #include <linux/linux_io_device.h>
+    #include "linux/linux_io_device.h"
 #endif
 
 #endif // ___cow_io_device_h___
